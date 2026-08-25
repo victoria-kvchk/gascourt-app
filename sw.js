40704cdc05b986c70894a4dfe8ed19bad06f8a77
victoria-kvchk <laurent.delteil0@gmail.com>
2026-08-25 14:15:31 +0200

    Gascourt · version 1cdd13df24c0

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
  *    arrière-plan. L'app s'ouvre instantanément et la mise à jour est prise au
  *    lancement suivant, ce qui vaut mieux qu'une attente au démarrage.
  */
-const VERSION = 'ec8fbdac1abf';
+const VERSION = '1cdd13df24c0';
 const RESERVE = 'gascourt-' + VERSION;
 const PIECES = ['./', './index.html', './manifest.webmanifest',
                 './icone-180.png', './icone-192.png', './icone-512.png'];
