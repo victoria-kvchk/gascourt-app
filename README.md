0adedb35ccea35e5dbb3ec6c4db6094c9270df43
victoria-kvchk <laurent.delteil0@gmail.com>
2026-08-25 11:53:37 +0200

    Gascourt · version d6e2a575dad0

--- README.md
+++ README.md
@@ -13,4 +13,4 @@ Le code source, les tests et le script d'assemblage sont tenus ailleurs, dans un
 dépôt privé : son historique porte des données personnelles, et l'on ne rend pas
 public un passé.
 
-Version en réserve : `cc20a501e54f`
+Version en réserve : `d6e2a575dad0`
--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
  *    arrière-plan. L'app s'ouvre instantanément et la mise à jour est prise au
  *    lancement suivant, ce qui vaut mieux qu'une attente au démarrage.
  */
-const VERSION = 'cc20a501e54f';
+const VERSION = 'd6e2a575dad0';
 const RESERVE = 'gascourt-' + VERSION;
 const PIECES = ['./', './index.html', './manifest.webmanifest',
                 './icone-180.png', './icone-192.png', './icone-512.png'];
